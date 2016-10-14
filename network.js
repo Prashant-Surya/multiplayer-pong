0822e9b16061ce106abcd845232f24e7df66a323
Surya Prashanth <prashantsurya@ymail.com>
2016-10-15 00:29:58 +0530

    Starting networking

---
+++ network.js
@@ -0,0 +1,13 @@
+var socket = io('http://localhost');
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+var player_id = guid();
+socket.emit('connect', {'player_name': player_name, 'player_id' : player_id});

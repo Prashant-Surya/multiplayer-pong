ae7f45d6e5cc9b40252f19746bf6ccce6cae285f
Surya Prashanth <prashantsurya@ymail.com>
2016-10-15 02:10:40 +0530

    Display active users

--- network.js
+++ network.js
@@ -1,3 +1,4 @@
+var player_name = prompt('Please enter your name');
 var socket = io('http://localhost:8123');
 function guid() {
   function s4() {
@@ -9,5 +10,18 @@ function guid() {
     s4() + '-' + s4() + s4() + s4();
 }
 
+var players = {};
 var player_id = guid();
-socket.emit('connect', {'player_name': player_name, 'player_id' : player_id});
+socket.emit('new_user', {'player_name': player_name, 'player_id' : player_id});
+
+function add_user(data) {
+    $('#active_players').append('<li id="' +data['player_id'] + '">' + data['player_name'] +"</li>" );
+}
+
+socket.on('new_user', function(data) {
+    players[data['player_id']] = data['player_name'];
+    console.log(data['player_name'] + ' logged in.');
+    add_user(data);
+
+});
+
--- server.js
+++ server.js
@@ -20,7 +20,22 @@ function handler (req, res) {
     res.end(data);
   });
 }
+var players = {};
 
-io.on('connect', function (socket) {
-  console.log('User connected');
+io.on('connection', function (socket) {
+  var cur_id;
+  socket.on('new_user', function(data) {
+    players[data['player_id']] = {'player_name' : data['player_name'], 'socket' : socket};
+    cur_id = data['player_id'];
+    socket.broadcast.emit('new_user', data);
+    console.log(players);
+  });
+
+
+  socket.on('disconnect', function() {
+    console.log(players[cur_id]['player_name'] + ' logged out');
+    delete players[cur_id];
+    console.log(players);
+  });
+
 });
